320ea01d35d035388bdbf2827309415fc5057fa9
paulliu87 <paul.pliu87@gmail.com>
2017-01-20 00:05:49 -0700

    Create controller and new page

--- app/controllers/lunchorders_controller.rb
+++ app/controllers/lunchorders_controller.rb
@@ -1,2 +1,5 @@
 class LunchordersController < ApplicationController
+	def new
+	end
+	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
-  resource :lunchorders
+  resources :lunchorders
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
