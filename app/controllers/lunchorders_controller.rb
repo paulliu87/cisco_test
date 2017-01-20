c2413de56e6cd0d80eb34a92314f4dccd529866b
paulliu87 <paul.pliu87@gmail.com>
2017-01-20 00:09:58 -0700

    Create controller create

--- app/controllers/lunchorders_controller.rb
+++ app/controllers/lunchorders_controller.rb
@@ -1,5 +1,17 @@
 class LunchordersController < ApplicationController
 	def new
 	end
-	
+
+	def create
+		@lunchorder = Lunchorder.new(lunchorcher.params)
+
+		@lunchorder.save
+		redirect_to @lunchorder
+	end
+
+	private
+		def lunchorder_params
+			params.require(:lunchorder).permit(:normal, :vegetarian, :gluten_free, :nut_free, :fish_free)
+		end
+
 end
