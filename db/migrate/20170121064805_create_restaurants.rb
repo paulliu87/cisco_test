05c61df703e280507201fdf7b718c4d6248a9c6c
paulliu87 <paul.pliu87@gmail.com>
2017-01-20 23:49:30 -0700

    Initialize the model

---
+++ db/migrate/20170121064805_create_restaurants.rb
@@ -0,0 +1,13 @@
+class CreateRestaurants < ActiveRecord::Migration[5.0]
+  def change
+    create_table :restaurants do |t|
+    	t.integer 	:rating
+		t.integer 	:normal
+		t.integer 	:vegetarian
+		t.integer 	:nut_free
+		t.integer 	:gluten_free
+		t.integer 	:fish_free
+      t.timestamps
+    end
+  end
+end
