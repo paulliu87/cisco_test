8b1bc250df5c73d07ba717666007fdbcd5279596
paulliu87 <paul.pliu87@gmail.com>
2017-01-20 22:34:45 -0700

    Add tests for show, new, and create

--- app/models/lunch_order.rb
+++ app/models/lunch_order.rb
@@ -1,13 +1,2 @@
 class LunchOrder < ApplicationRecord
-	def change
-		create_table 	:lunchorders do |t|
-			t.integer 	:normal
-			t.integer 	:vegetarian
-			t.integer 	:nut_free
-			t.integer 	:gluten_free
-			t.integer 	:fish_free
-
-			t.timestamps
-		end
-	end
 end
