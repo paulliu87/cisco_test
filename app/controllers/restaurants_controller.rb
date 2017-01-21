3e92ff4231704f10a0af7bfdded7f82c73d8728a
paulliu87 <paul.pliu87@gmail.com>
2017-01-20 23:46:42 -0700

    Create routes and erb files

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,30 @@
 class RestaurantsController < ApplicationController
+	def show
+		@restaurant = Restaurant.find(params[:id])
+	end
+
+	def new
+	end
+
+	def create
+		convert_string_to_integer(restaurant_params)
+		@restaurant = Restaurant.new(convert_string_to_integer(restaurant_params))
+		@restaurant.save
+		redirect_to "/restaurants/#{@restaurant.id}"
+	end
+
+	private
+		def restaurant_params
+			params.require(:restaurant).permit( :normal, :vegetarian, :gluten_free, :nut_free, :fish_free)
+		end
+
+		def convert_string_to_integer(params)
+			{
+				:normal => params[:normal].to_i,
+				:vegetarian => params[:vegetarian].to_i,
+				:nut_free => params[:nut_free].to_i,
+				:gluten_free => params[:gluten_free].to_i,
+				:fish_free => params[:fish_free].to_i,
+			}
+		end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   root 'home#index'
 
   resources :lunchorders
+  resources :restaurants
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
