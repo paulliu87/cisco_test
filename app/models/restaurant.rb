1c3d4bd12891798fe219ed5c080eeaea3ba93854
paulliu87 <paul.pliu87@gmail.com>
2017-01-23 02:53:46 -0700

    Add validationg to restaurants

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,23 @@
 class Restaurant < ApplicationRecord
+	validates_presence_of :name, :rating
+	validates_uniqueness_of :name
+	validate :at_least_one_meal, on: :create
+
+	after_initialize :init
+
+    def init
+      self.normal  ||= 0
+      self.vegetarian ||= 0
+      self.nut_free ||= 0
+      self.gluten_free  ||= 0
+      self.fish_free ||= 0
+    end
+
+	def at_least_one_meal
+		if (self.normal + self.vegetarian + self.nut_free + self.gluten_free + self.fish_free) <= 0
+			errors.add(:storage, "must have at least one meal.")
+			false
+		end
+	end
+
 end
